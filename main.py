45b1461f9acf0c1f21c6f7206e4300cc02110dd1
miche <michelle.dardon@ufm.edu>
2023-05-01 23:06:44 -0600

    Benchmark
    benchmarking bst vs avl

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ bst = BinarySearchTree()
 print("--------- Inserting elements to the Tree ---------")
 
 def random_nodes(bst: BinarySearchTree, avl: AVLTree):
-    numbers = random.sample(range(1, 21), 20)
-    root = None
+    numbers = random.sample(range(1, 1000001), 1000000)
+    
     for num in numbers:
         bst.insert(num)
         avl.insert(num)
@@ -23,38 +23,42 @@ random_nodes(bst, avl)
 print("--------- Traversing Tree ---------")
 #bst.traverse(bst.root)
 
-'''
+
 # Search
 print("--------- Searching keys in the Trees ---------")
-test_keys = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+test_numbers = random.sample(range(1, 1000001), 350000)
 
 #BENCHMARK SEARCH FUNCTION
-#start_time= time.time()
-for key in test_keys:
+start_time= time.time()
+for key in test_numbers:
     print('Searching for {}: {}'.format(key, avl.search(key)))
 end_time = time.time()
-elapsed_time = end_time - start_time
-print(f"Elapsed time in ALV TREE: {elapsed_time:.6f} seconds")
+elapsed_time1 = end_time - start_time
+print(f"Elapsed time in AVL TREE: {elapsed_time1:.6f} seconds")
 
-print(avl)
+#print(avl)
 
 print("-------------------------------")
 
 start_time= time.time()
-for key in test_keys:
+for key in test_numbers:
     print('Searching for {}: {}'.format(key, bst.search(key)))
 end_time = time.time()
-elapsed_time = end_time - start_time
-print(f"Elapsed time in BST TREE: {elapsed_time:.6f} seconds")
-print(bst)
+elapsed_time2 = end_time - start_time
+print(f"Elapsed time in BST TREE: {elapsed_time2:.6f} seconds")
+#print(bst)
 
+diferencia = elapsed_time2 - elapsed_time1
+print("La diferencia fue de: ", diferencia)
+'''
 # Min-Max 
 print("--------- Searching for min-max in Trees ---------")
 
 
 # Delete
 print("--------- Deleting elements from the Trees ---------")
-'''
+
 
 print("--------- Visualization of the Trees ---------")
 
@@ -65,3 +69,4 @@ dot.render("AVL-TREE.gv", view=True)
 #bst.traverse(bst.root)
 bst.traverse_and_add_nodes_to_the_graph(bst.root)
 bst.display()
+'''
